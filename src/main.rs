30693ca2c5a66c75b913d531b75856cec4eaf0ad
aRustyDev <developer@arusty.dev>
2024-07-12 13:22:45 -0400

    WIP; compiles successfully

--- src/main.rs
+++ src/main.rs
@@ -7,97 +7,97 @@ use std::io::Error;
 use serde::{Serialize, Deserialize};
 use clap::Parser;
 use serde_yaml;
-use libc::pid_t;
+// use libc::pid_t;
 // use reqwest;
-use std::any::type_name;
+// use std::any::type_name;
 use tempfile::Builder;
 
-const DEFAULT_CRX_URL: &str     = "https://clients2.google.com/service/update2/crx";
-const DEFAULT_CRX_CONFIG: &str  = "./crx.yaml";
+// const DEFAULT_CRX_URL: &str     = "https://clients2.google.com/service/update2/crx";
+// const DEFAULT_CRX_CONFIG: &str  = "./crx.yaml";
 
-struct Chrome<'a> {
-    pid: pid_t,
-    crx: Vec<String>,
-    url: &'a str,
-}
+// struct Chrome<'a> {
+//     pid: pid_t,
+//     crx: Vec<String>,
+//     url: &'a str,
+// }
 
-impl Chrome<'_> {
-    fn new() -> Chrome<'static> { // TODO: plumb URL option & crx read from yaml
-        Chrome {
-            pid: 0,
-            crx: Vec::new(),
-            url: "https://clients2.google.com/service/update2/crx",
-        }
-    }
+// impl Chrome<'_> {
+//     fn new() -> Chrome<'static> { // TODO: plumb URL option & crx read from yaml
+//         Chrome {
+//             pid: 0,
+//             crx: Vec::new(),
+//             url: "https://clients2.google.com/service/update2/crx",
+//         }
+//     }
 
-    // fn download(&self) -> Result<(), E> {
-    //     let tmp_dir = Builder::new().prefix("example").tempdir()?;
-    //     let response = reqwest::get(self.url).await?;
+//     // fn download(&self) -> Result<(), E> {
+//     //     let tmp_dir = Builder::new().prefix("example").tempdir()?;
+//     //     let response = reqwest::get(self.url).await?;
 
-    //     let mut dest = {
-    //         let fname = response
-    //             .url()
-    //             .path_segments()
-    //             .and_then(|segments| segments.last())
-    //             .and_then(|name| if name.is_empty() { None } else { Some(name) })
-    //             .unwrap_or("tmp.bin");
+//     //     let mut dest = {
+//     //         let fname = response
+//     //             .url()
+//     //             .path_segments()
+//     //             .and_then(|segments| segments.last())
+//     //             .and_then(|name| if name.is_empty() { None } else { Some(name) })
+//     //             .unwrap_or("tmp.bin");
 
-    //         println!("file to download: '{}'", fname);
-    //         let fname = tmp_dir.path().join(fname);
-    //         println!("will be located under: '{:?}'", fname);
-    //         File::create(fname)?
-    //     };
-    //     let content =  response.text().await?;
-    //     copy(&mut content.as_bytes(), &mut dest)?;
-    //     Ok(())
+//     //         println!("file to download: '{}'", fname);
+//     //         let fname = tmp_dir.path().join(fname);
+//     //         println!("will be located under: '{:?}'", fname);
+//     //         File::create(fname)?
+//     //     };
+//     //     let content =  response.text().await?;
+//     //     copy(&mut content.as_bytes(), &mut dest)?;
+//     //     Ok(())
 
-    //     let crx = Command::new("wget")
-    //                       .arg(self.url)
-    //                       .output().unwrap();
-    //     println!("CRX: {}", String::from_utf8_lossy(&crx.stdout));
-    // }
+//     //     let crx = Command::new("wget")
+//     //                       .arg(self.url)
+//     //                       .output().unwrap();
+//     //     println!("CRX: {}", String::from_utf8_lossy(&crx.stdout));
+//     // }
 
-    // fn start(&self) -> Result<(), E> { // TODO return chrome process
+//     // fn start(&self) -> Result<(), E> { // TODO return chrome process
 
-    //     // Windows: $chromePath = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" # Path to Chrome executable
-    //     // Linux: $chromePath = "/usr/bin/google-chrome" # Path to Chrome executable
-    //     // Mac: $chromePath = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" # Path to Chrome executable
+//     //     // Windows: $chromePath = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" # Path to Chrome executable
+//     //     // Linux: $chromePath = "/usr/bin/google-chrome" # Path to Chrome executable
+//     //     // Mac: $chromePath = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" # Path to Chrome executable
 
-    //     // Path to Chrome application (adjust if necessary)
-    //     let chrome_path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+//     //     // Path to Chrome application (adjust if necessary)
+//     //     let chrome_path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
-    //     // Command to launch Chrome with remote debugging
-    //     let mut command = Command::new(chrome_path);
-    //     command.arg("--remote-debugging-port=9222");
+//     //     // Command to launch Chrome with remote debugging
+//     //     let mut command = Command::new(chrome_path);
+//     //     command.arg("--remote-debugging-port=9222");
 
-    //     // Attempt to spawn the process
-    //     let process = match command.spawn() {
-    //         Ok(child) => child,
-    //         Err(err) => {
-    //             eprintln!("Error spawning Chrome process: {}", err);
-    //             return;
-    //         }
-    //     };
+//     //     // Attempt to spawn the process
+//     //     let process = match command.spawn() {
+//     //         Ok(child) => child,
+//     //         Err(err) => {
+//     //             eprintln!("Error spawning Chrome process: {}", err);
+//     //             return;
+//     //         }
+//     //     };
 
-    //     // Get the process ID
-    //     let self.pid = unsafe { process.id() } as pid_t;
+//     //     // Get the process ID
+//     //     let self.pid = unsafe { process.id() } as pid_t;
 
-    //     // Print success message and PID
-    //     println!("Chrome launched in remote debugging mode with PID: {}", self.pid);
-    //     Ok(())
-    // }
+//     //     // Print success message and PID
+//     //     println!("Chrome launched in remote debugging mode with PID: {}", self.pid);
+//     //     Ok(())
+//     // }
 
-    // fn kill(&self) -> Result<(), E> {
-    //     unsafe {
-    //         libc::kill(self.pid, libc::SIGTERM);
-    //     }
-    //     println!("Chrome process terminated (PID: {})", self.pid);
-    //     // Safety note: This function is not implemented as directly killing a process
-    //     // can be dangerous. You should implement proper logic with error handling
-    //     // based on your specific needs.
-    // }
+//     // fn kill(&self) -> Result<(), E> {
+//     //     unsafe {
+//     //         libc::kill(self.pid, libc::SIGTERM);
+//     //     }
+//     //     println!("Chrome process terminated (PID: {})", self.pid);
+//     //     // Safety note: This function is not implemented as directly killing a process
+//     //     // can be dangerous. You should implement proper logic with error handling
+//     //     // based on your specific needs.
+//     // }
 
-}
+// }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct CrxYml {
@@ -114,10 +114,10 @@ impl<'a> CrxYml {
         }
     }
 
-    fn init(&self, path: &str) {
+    fn init(&mut self, path: &str) {
         let contents = fs::read_to_string(path)
             .expect("Should have been able to read the file");
-        self = &serde_yaml::from_str::<CrxYml>(&contents).unwrap();
+        *self = serde_yaml::from_str::<CrxYml>(&contents).unwrap();
     }
 
     async fn download_crx(&self, crx_id: &str, tmp: &str) -> Result<(), Box<dyn std::error::Error>>{
@@ -137,7 +137,7 @@ impl<'a> CrxYml {
     }
 
     // Assumes CrxYml is
-    fn install(&self, path: &str) -> Result<(), Error> {
+    fn install(&mut self, path: &str) -> Result<(), Error> {
         // 1. Read Config CRX.yaml @ path
         // self.crx = read_config(path);
         self.init(path);
@@ -146,8 +146,8 @@ impl<'a> CrxYml {
 
         // 2. Download CRX from Chrome Web Store
         let map = self.crx.clone();
-        let tmp = Builder::new().prefix("example").tempdir()?;
-        for (name, id) in map.into_iter() {
+        let _tmp = Builder::new().prefix("example").tempdir()?;
+        for (name, _id) in map.into_iter() {
             println!("Downloading CRX: {}", name);
             // self.download_crx(id, tmp);
             // map.remove(name);
@@ -164,7 +164,7 @@ impl<'a> CrxYml {
         Ok(())
     }
 
-    fn export(&self, path: &str) -> Result<(), Error> {
+    fn export(&self, _path: &str) -> Result<(), Error> {
         println!("You ran '{:#?}'!", "export");
         println!("Not Implemented Yet!");
         Ok(())
@@ -173,7 +173,7 @@ impl<'a> CrxYml {
         // 3. Pack Extension
     }
 
-    fn load(&self, path: &str) -> Result<(), Error> {
+    fn load(&self, _path: &str) -> Result<(), Error> {
         println!("You ran '{:#?}'!", "export");
         println!("Not Implemented Yet!");
         Ok(())
@@ -183,7 +183,7 @@ impl<'a> CrxYml {
         // 4. Install Extensions
     }
 
-    fn cleanup(&self, path: &str) -> Result<(), Error> {
+    fn cleanup(&self, _path: &str) -> Result<(), Error> {
         println!("You ran '{:#?}'!", "export");
         println!("Not Implemented Yet!");
         Ok(())
@@ -206,9 +206,9 @@ impl<'a> CrxYml {
 //     println!("CONNECT: {}", String::from_utf8_lossy(&connect.stdout));
 // }
 
-fn type_of<T>(_: T) -> &'static str {
-    type_name::<T>()
-}
+// fn type_of<T>(_: T) -> &'static str {
+//     type_name::<T>()
+// }
 
 // fn read_config(path: &str) -> Vec<String> {
 //     // Open the file in read-only mode with buffer.
@@ -258,7 +258,7 @@ fn query_user() {
 /// CLI tool to install & administer Chrome Extensions via a headless Chrome instance.
 #[derive(Clone, Parser, Debug)]
 #[command(version, about, author, after_help = "", long_about = None)]
-struct Args<'a> {
+struct Args {
 
     /// Install new Chrome extensions described in the CRX.yaml.
     #[arg(short, long)]
@@ -280,9 +280,9 @@ struct Args<'a> {
     #[arg(short, long)]
     update: bool,
 
-    /// Export currently installed Chrome extensions as a Zipped CRX file
-    #[arg(short, long, value_parser, default_value_t = "../data/crx.yaml")]
-    path: &'a str,
+    /// Export currently installed Chrome extensions as a Zipped CRX file.
+    #[arg(short, long, default_value_t = String::from("../data/crx.yaml"))]
+    path: String,
 }
 
 fn main() -> Result<(), Error> {
@@ -292,9 +292,9 @@ fn main() -> Result<(), Error> {
     //     .literal(styling::AnsiColor::Blue.on_default() | styling::Effects::BOLD)
     //     .placeholder(styling::AnsiColor::Cyan.on_default());
     let args = Args::parse();
-    let crx_yml = CrxYml::new();
+    let mut crx_yml = CrxYml::new();
 
-    match args {
+    let _ = match args {
         Args { install: true, .. } => crx_yml.install(&args.path),
         // Args { cleanup: true, .. } => crx_yml.cleanup(&args.path),
         // Args { load: _, .. } => crx_yml.load(&args.path),
